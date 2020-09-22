10f49fc3eb97e9347037cdd93182d7af7bf7c126
Sciroccogti <scirocco_gti@yeah.net>
2020-09-22 11:02:31 +0800

    able to use checkpoint

--- Model_train.py
+++ Model_train.py
@@ -9,9 +9,7 @@ import random
 import h5py
 import numpy as np
 import torch
-import torch.nn as nn
-from Model_define_pytorch import (AutoEncoder, DatasetFolder, NMSE_cuda,
-                                  NMSELoss)
+from Model_define_pytorch import (AutoEncoder, DatasetFolder, NMSELoss)
 
 # Parameters for training
 gpu_list = '0'
@@ -30,7 +28,7 @@ def seed_everything(seed=42):
 SEED = 42
 seed_everything(SEED)
 
-batch_size = 400
+batch_size = 1024
 epochs = 100
 learning_rate = 2e-3  # bigger to train faster
 num_workers = 4
@@ -42,12 +40,24 @@ img_height = 16
 img_width = 32
 img_channels = 2
 
+resume = 'Modelsave/'
+# resume = None
 
 # Model construction
 model = AutoEncoder(feedback_bits)
+startepoch = 0
 
 model.encoder.quantization = False
 model.decoder.quantization = False
+
+if resume is not None:
+    checkpoint_encoder = torch.load(resume + 'encoder.pth.tar')
+    checkpoint_decoder = torch.load(resume + 'decoder.pth.tar')
+    startepoch = checkpoint_encoder['epoch'] + 1
+    model.encoder.load_state_dict(checkpoint_encoder['state_dict'])
+    model.decoder.load_state_dict(checkpoint_decoder['state_dict'])
+    print('checkpoint loaded!')
+
 
 if len(gpu_list.split(',')) > 1:
     model = torch.nn.DataParallel(model).cuda()  # model.module
@@ -81,7 +91,7 @@ test_dataset = DatasetFolder(x_test)
 test_loader = torch.utils.data.DataLoader(
     test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
 best_loss = 100
-for epoch in range(epochs):
+for epoch in range(startepoch, epochs):
     print('========================')
     print('lr:%.4e' % optimizer.param_groups[0]['lr'])
     # model training
@@ -139,7 +149,7 @@ for epoch in range(epochs):
             # save encoder
             modelSave1 = './Modelsave/encoder.pth.tar'
             torch.save(
-                {'state_dict': model.encoder.state_dict(), }, modelSave1)
+                {'state_dict': model.encoder.state_dict(), 'epoch': epoch, }, modelSave1)
             # save decoder
             modelSave2 = './Modelsave/decoder.pth.tar'
             torch.save(
